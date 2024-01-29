5f827bbdd72379cab88941472753515009cc604d
Jayne Cruz <152091678+jayne-cruz@users.noreply.github.com>
2024-01-29 10:29:49 -0300

    Update Program.cs

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -1,30 +1,33 @@
+// Importa a classe Estacionamento do namespace DesafioFundamentos.Models
 using DesafioFundamentos.Models;
 
-// Coloca o encoding para UTF8 para exibir acentuação
+// Configura a codificação de saída para UTF-8 para exibir caracteres especiais
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+// Declaração de variáveis para adicionar preços do estacionamento
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
 
-Console.WriteLine("Seja bem vindo ao Estacionamento Central Park!\n" +
-                  "Digite o preço inicial:");
+// Exibe uma mensagem de boas-vindas e solicita o preço inicial ao usuário
+Console.WriteLine("Seja bem-vindo ao Estacionamento Central Park!\n" +
+                  "Digite o preço de entrada:");
 precoInicial = Convert.ToDecimal(Console.ReadLine());
 
+// Solicita o preço por hora
 Console.WriteLine("Agora digite o preço por hora:");
 precoPorHora = Convert.ToDecimal(Console.ReadLine());
 
-// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
+// Instancia a classe Estacionamento, passando os valores obtidos do usuário
 Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
 
+// Início das variáveis para controlar o menu de opções
 string opcao = string.Empty;
 bool exibirMenu = true;
 
-// Realiza o loop do menu
-/*Aqui adicionei While e Switch para processar a entrada e interligar os métodos correspondentes
-da classe Estacionamento*/
+// Loop principal do menu
 while (exibirMenu)
 {
-    // Limpa a tela e exibe as opções do menu
+    // Limpa a tela do usuário e exibe as opções do menu
     Console.Clear();
     Console.WriteLine("Digite a sua opção:");
     Console.WriteLine("1 - Cadastrar veículo");
@@ -32,31 +35,39 @@ while (exibirMenu)
     Console.WriteLine("3 - Listar veículos");
     Console.WriteLine("4 - Encerrar");
 
+    // Obtém a escolha do usuário e executa a ação correspondente usando switch
     switch (Console.ReadLine())
     {
         case "1":
+            // Chama o método AdicionarVeiculo da instância da classe Estacionamento
             es.AdicionarVeiculo();
             break;
 
         case "2":
+            // Chama o método RemoverVeiculo da instância da classe Estacionamento
             es.RemoverVeiculo();
             break;
 
         case "3":
+            // Chama o método ListarVeiculos da instância da classe Estacionamento
             es.ListarVeiculos();
             break;
 
         case "4":
+            // Encerra o loop do menu definindo exibirMenu como falso
             exibirMenu = false;
             break;
 
         default:
+            // Mensagem de "opção inválida" caso usuário insira algo fora do esperado
             Console.WriteLine("Opção inválida. Digite uma nova opção");
             break;
     }
 
+    // Aguarda que o usuário pressione uma tecla para continuar
     Console.WriteLine("Pressione uma tecla para continuar");
     Console.ReadLine();
 }
 
-Console.WriteLine("Obrigado por usar nossos serviços!");
+// Mensagem final de agradecimento ao usuário
+Console.WriteLine("Obrigado por usar nossos serviços. Volte sempre!");
